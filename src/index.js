3e3749d3fdaf6baddd53e282c1497d34e48bf928
Zaniuk <61323637+Zaniuk@users.noreply.github.com>
2022-07-18 18:24:39 -0300

    added use of body parser added welcome message added operations route

--- src/index.js
+++ src/index.js
@@ -1,15 +1,19 @@
 require('dotenv').config()
+const bodyParser = require('body-parser');
 const express = require('express');
 const app = express();
-const homeRoute = require('./routes/home')
 const loginRoute = require('./routes/login')
 const registerRoute = require('./routes/register')
+const operationsRoute = require('./routes/operations')
 
-// respond with "hello world" when a GET request is made to the homepage
-app.use('/', homeRoute)
+app.use(bodyParser.json())
+
+app.get('/', (_req, res) => {
+    res.send('Welcome to my API')
+})
 app.use('/login', loginRoute)
 app.use('/register', registerRoute)
-
+app.use('/operations', operationsRoute)
 app.listen(3000, ()=>{
     console.log('Connected!')
 })

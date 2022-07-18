2cd768ad09107c91ea557736906fa49449c678e5
Zaniuk <61323637+Zaniuk@users.noreply.github.com>
2022-07-18 18:24:02 -0300

    register route created

--- src/routes/register.js
+++ src/routes/register.js
@@ -1,8 +1,33 @@
 const mongoose = require('mongoose')
+
 const router = require('express').Router()
+const User = require('../models/user')
 const DB_URL = process.env.DB_URL
 
-router.get('/', (_req, res) =>{
-    res.send('getting register route')
-} )
+
+mongoose.connect(DB_URL, ()=> {
+    console.log('DB is connected')
+})
+
+
+router.post('/', async( req, res ) =>{
+    //important note: NEVER save user credentials as raw strings.
+    const request = req.body
+    const user = new User({
+        username: request.username,
+        email: request.email,
+        password: request.password
+    })
+    try{
+        await user.save()
+        res.send({message: 'User created sucessfully'})
+    }
+    catch(err){
+        if(err.code === 11000){
+            res.send({message: `${Object.keys(err.keyValue)} already exists`})
+        }else{
+            res.send(err)
+        }
+    }
+ })
 module.exports = router;

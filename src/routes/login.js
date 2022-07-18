43e3768eddb6d3608283a2e462cf1f06290c1f3e
Zaniuk <61323637+Zaniuk@users.noreply.github.com>
2022-07-18 18:26:47 -0300

    added login route important note: change passing RAW user data before pushing into the challenge

--- src/routes/login.js
+++ src/routes/login.js
@@ -1,8 +1,20 @@
-
 const router = require('express').Router()
-
-
-router.get('/', (_req, res) =>{
-    res.send('getting login route')
+const User = require('../models/user')
+const Operation = require('../models/operation')
+router.post('/',async (req, res) =>{
+    const request = req.body
+    const user = await User.findOne({"email" : request.email, "passwod": request.passwod})
+    if(user.email === request.email && user.password === request.password){
+        
+       
+        // const list = await Operation.find({user: user.id})
+        // Horrendous approach, NEVER send this decrypted, this is only for testing and mock purposes
+        res.set({
+            'user-token' : user._id
+        })
+        res.send(user)
+    }else{
+        res.send('Error not valid email or password')
+    }
 } )
 module.exports = router;

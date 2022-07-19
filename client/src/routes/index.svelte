85b8109b8e1a33a85bdb7253faa948297cd65d04
Zaniuk <61323637+Zaniuk@users.noreply.github.com>
2022-07-19 16:30:37 -0300

    Svelte boilerplate

---
+++ client/src/routes/index.svelte
@@ -0,0 +1,10 @@
+<h1>Welcome to SvelteKit</h1>
+<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script>
+const options = {method: 'GET', headers: {user: '62d34380b3954d8270d05ea2'}};
+
+fetch('http://localhost/operations', options)
+  .then(response => response.json())
+  .then(response => console.log(response))
+  .catch(err => console.error(err));
+</script>

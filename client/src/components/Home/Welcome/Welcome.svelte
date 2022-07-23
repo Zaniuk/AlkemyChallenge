df5667c7ba55423779a5a7359a2cbf901dda223e
Zaniuk <61323637+Zaniuk@users.noreply.github.com>
2022-07-23 15:06:22 -0300

    Started working on Welcome page (WIP)

---
+++ client/src/components/Home/Welcome/Welcome.svelte
@@ -0,0 +1,18 @@
+<script>
+    async function getBalance(){
+    const options = {method: 'GET', headers: {user: '62d34380b3954d8270d05ea2'}};        
+    const data = await fetch('http://localhost/operations', options).then(response => response.json())
+    
+    let balance = 0
+    data.forEach(el => {
+        balance += el.amount
+    });
+    return balance
+}
+</script>
+<h1>Hola Geronimo</h1>
+{#await getBalance()}
+{:then balance} 
+<h1>Tu balance es: ${balance}</h1>
+    
+{/await}

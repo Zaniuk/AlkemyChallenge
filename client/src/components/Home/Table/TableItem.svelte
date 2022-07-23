69706f864c80faef80581e526dc5f3b1d709a3c0
Zaniuk <61323637+Zaniuk@users.noreply.github.com>
2022-07-23 15:04:44 -0300

    Added operations as a table component

---
+++ client/src/components/Home/Table/TableItem.svelte
@@ -0,0 +1,20 @@
+<script>
+    import { goto } from '$app/navigation';
+// @ts-nocheck  
+    export let concept, amount, date, type, id;
+    const edit = () => {
+        goto(`/edit/${id}`)
+    }
+</script>
+
+<tr>
+    <td>{`${date.getDate()}-${date.getMonth()+1}-${date.getFullYear()}`}</td>
+    <td>{type}</td>
+    <td>{concept}</td>  
+    <td>{amount}</td>
+    <td><button on:click={edit}>Editar</button><button>Eliminar</button></td>
+    
+</tr>
+<style>
+
+</style>

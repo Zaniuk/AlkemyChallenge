6e72b7e322d7a59c8271d4b432dfcf9e673ce362
Zaniuk <61323637+Zaniuk@users.noreply.github.com>
2022-07-23 18:45:37 -0300

    added functionability to remove button

--- client/src/components/Home/Table/TableItem.svelte
+++ client/src/components/Home/Table/TableItem.svelte
@@ -1,7 +1,15 @@
 <script>
-    import { goto } from '$app/navigation';
-// @ts-nocheck  
     export let concept, amount, date, type, id;
+    import { goto } from '$app/navigation';
+    import {removeOne} from '../../../hooks/dataHandling/removeOne'
+// @ts-nocheck  
+    const removeItem = async() => {
+        if(confirm('Estas seguro de querer borrar esta operacion?')){
+            removeOne(id)
+            location.reload()
+        }
+    }
+    
     const edit = () => {
         goto(`/edit/${id}`)
     }
@@ -12,7 +20,7 @@
     <td>{type}</td>
     <td>{concept}</td>  
     <td>{amount}</td>
-    <td><button on:click={edit}>Editar</button><button>Eliminar</button></td>
+    <td><button on:click={edit}>Editar</button><button on:click={removeItem}>Eliminar</button></td>
     
 </tr>
 <style>

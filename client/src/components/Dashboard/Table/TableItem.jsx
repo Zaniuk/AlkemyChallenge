10bacdf30778945f2a7d63c5dae64edd2be0a541
Zaniuk <61323637+Zaniuk@users.noreply.github.com>
2022-07-31 18:28:17 -0300

    passing operation.type to url

--- client/src/components/Dashboard/Table/TableItem.jsx
+++ client/src/components/Dashboard/Table/TableItem.jsx
@@ -9,7 +9,7 @@ export default function TableItem({operation}) {
             <td>{operation.type}</td>
             <td>{operation.concept}</td>  
             <td>{operation.amount}</td>
-            <td><a href={`edit/${operation._id}`}><button>Editar</button></a><button className='delete-button'>Eliminar</button></td>
+            <td><a href={`edit/${operation._id}&${operation.type}`}><button>Editar</button></a><button className='delete-button'>Eliminar</button></td>
         </tr>
       )
   }

0edbed4fb36798a784be7cd1f60e3464e07a4d9e
Zaniuk <61323637+Zaniuk@users.noreply.github.com>
2022-07-31 17:03:04 -0300

    Added sucess route

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -7,6 +7,7 @@ import Edit from './routes/Edit';
 import Create from './routes/Create';
 import NotFound from './routes/NotFound';
 import './assets/pico.min.css'
+import EditSucess from './components/EditOperation/EditSucess';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
@@ -15,6 +16,7 @@ ReactDOM.createRoot(document.getElementById('root')).render(
         <Route path='/' element={<Home/>} />
         <Route path='/edit/:id' element={<Edit/>} />
         <Route path='/create' element={<Create/>} />
+        <Route path='/sucess' element={<EditSucess/>}/>
         <Route path='*' element={<NotFound/>} />
       </Routes>
     </BrowserRouter>

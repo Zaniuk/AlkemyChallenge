f0008414b0d63dd4e07d8d22f13aa09a45c3bdf5
Zaniuk <61323637+Zaniuk@users.noreply.github.com>
2022-07-31 18:31:31 -0300

    added getOne, updateOne, and createOpr helpers

--- client/src/helpers/helpers.js
+++ client/src/helpers/helpers.js
@@ -7,6 +7,34 @@ export const getAll = async(user) => {
     const data = await fetch('http://localhost/operations', options).then(response => response.json())
     return data
 }
+export const getOne = async (id) => {
+    const data = await fetch(`http://localhost/operations/${id}`).then(response => response.json())
+    return data
+}
+export const updateOne = async({id, date, concept, amount}) => {
+    const rawData = {date, concept, amount}
+    const data = JSON.stringify(rawData)
+    const options = {
+        method: 'PUT',
+        headers: {'Content-Type': 'application/json'},
+        body: `{"id":"${id}","data":${data}}`
+      };
+      
+      const response = await fetch('http://localhost/operations', options).then(response => response.json())
+      return response
+}
+export const createOpr = async({concept, amount, type}) =>{
+    const user = '62d34380b3954d8270d05ea2'
+    const options = {
+        method: 'POST',
+        headers: {'Content-Type': 'application/json'},
+        body: `{"concept":"${concept}","amount":${amount},"type":"${type}","user":"${user}"}`
+      };
+      
+      const res = fetch('http://localhost/operations', options).then(response => response.json())
+      return res
+
+}
 const sortOperations = (operations) => {
     const data = operations.sort((a,b) => {
         if(a.date < b.date) return 1

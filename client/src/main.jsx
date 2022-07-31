68bcfb5f6c93b3560ca609cd311f9efa59090b39
Zaniuk <61323637+Zaniuk@users.noreply.github.com>
2022-07-30 22:15:28 -0300

    added routes and Home route

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,10 +1,19 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App'
-import './index.css'
+import { BrowserRouter, Routes, Route  } from "react-router-dom";
+import Home from './routes/Home'
+import Edit from './routes/Edit';
+import Create from './routes/Create';
+import './assets/pico.min.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<Home/>} />
+        <Route path='/edit/:id' element={<Edit/>} />
+        <Route path='/create' element={<Create/>} />
+      </Routes>
+    </BrowserRouter>
   </React.StrictMode>
 )
---
+++ client/src/routes/Home.jsx
@@ -0,0 +1,16 @@
+
+import '../App.css'
+import Table from '../components/Dashboard/Table/Table'
+import Balance from '../components/Dashboard/Balance/Balance'
+
+function Home() {
+  
+  return (
+    <div className="App container">
+      <Balance/>
+      <Table/>
+    </div>
+  )
+}
+
+export default Home

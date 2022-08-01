79b3b61b99750511b387813089bd059a4eeea286
Zaniuk <61323637+Zaniuk@users.noreply.github.com>
2022-08-01 19:14:00 -0300

    added operations controller template

---
+++ server/src/controllers/operations.controller.js
@@ -0,0 +1,18 @@
+export const getOperations = (req, res) => {
+    res.send('Getting Operations')
+}
+
+export const createOperation = (req, res) => {
+    res.send('creating Operation')
+}
+
+export const updateOperation = (req, res) => {
+    res.send('updating operation')
+}
+
+export const deleteOperation = (req, res) => {
+    res.send('deleting operation')
+}
+export const getOperationById = (req, res) => {
+    res.send('Getting Operation')
+}

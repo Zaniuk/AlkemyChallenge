10299f9b6bc8930bfef55bdee245fb7d9c09bb28
Zaniuk <61323637+Zaniuk@users.noreply.github.com>
2022-08-01 18:56:09 -0300

    First version of User model

---
+++ server/src/models/User.js
@@ -0,0 +1,29 @@
+import { DataTypes } from 'sequelize'
+import { sequelize } from "../database/database.js";
+import { Operation } from './Operation.js';
+
+export const User = sequelize.define('users', {
+    username: {
+        type: DataTypes.STRING,
+        unique: true
+    },
+    email: {
+        type: DataTypes.STRING
+    },
+    password: {
+        type: DataTypes.STRING
+    },
+    id: {
+        type: DataTypes.UUID,
+        primaryKey: true
+    },
+})
+User.hasMany(Operation, {
+    foreignKey: 'userId',
+    sourceKey: 'id'
+})
+
+Operation.belongsTo(User, {
+    foreignKey: 'userId',
+    targetId: 'id'
+})

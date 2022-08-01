6f6e85ecb878788552b61da0d1eda55a0c890043
Zaniuk <61323637+Zaniuk@users.noreply.github.com>
2022-07-31 21:23:00 -0300

    Added deleteOperation functionability

--- client/src/components/Dashboard/Table/TableItem.jsx
+++ client/src/components/Dashboard/Table/TableItem.jsx
@@ -1,16 +1,27 @@
 import React, { useEffect, useState } from 'react'
-import { convertDateToStriing } from '../../../helpers/helpers'
-export default function TableItem({operation}) {
-  if(operation){
+import { convertDateToStriing, deleteOperation } from '../../../helpers/helpers'
+export default function TableItem({ operation }) {
+  if (operation) {
     const date = convertDateToStriing(operation.date)
     return (
-        <tr id={operation._id}>
-            <td>{date}</td>
-            <td>{operation.type}</td>
-            <td>{operation.concept}</td>  
-            <td>{operation.amount}</td>
-            <td><a href={`edit/${operation._id}&${operation.type}`}><button>Editar</button></a><button className='delete-button'>Eliminar</button></td>
-        </tr>
-      )
+      <tr id={operation._id}>
+        <td>{date}</td>
+        <td>{operation.type}</td>
+        <td>{operation.concept}</td>
+        <td>{operation.amount}</td>
+        <td>
+          <a href={`edit/${operation._id}&${operation.type}`}>
+            <button>Editar</button>
+          </a>
+          <button
+            onClick={() => {
+              deleteOperation(operation._id).then(location.reload())
+            }}
+            className='delete-button'>
+            Eliminar
+          </button>
+        </td>
+      </tr>
+    )
   }
 }

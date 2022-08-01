7aa13bea06763e46fb8a98a0ab9104b7b8b1e68d
Zaniuk <61323637+Zaniuk@users.noreply.github.com>
2022-07-31 21:21:34 -0300

    added deleteOperation helper

--- client/src/helpers/helpers.js
+++ client/src/helpers/helpers.js
@@ -1,9 +1,9 @@
 export const convertDateToStriing = (d) => {
     const date = new Date(d)
-    return `${date.getFullYear()}-${date.getMonth()+1}-${date.getDate()}`
+    return `${date.getFullYear()}-${date.getMonth() + 1}-${date.getDate()}`
 }
-export const getAll = async(user) => {
-    const options = {method: 'GET', headers: {user: user}};
+export const getAll = async (user) => {
+    const options = { method: 'GET', headers: { user: user } };
     const data = await fetch('http://localhost/operations', options).then(response => response.json())
     return data
 }
@@ -11,52 +11,60 @@ export const getOne = async (id) => {
     const data = await fetch(`http://localhost/operations/${id}`).then(response => response.json())
     return data
 }
-export const updateOne = async({id, date, concept, amount}) => {
-    const rawData = {date, concept, amount}
+export const updateOne = async ({ id, date, concept, amount }) => {
+    const rawData = { date, concept, amount }
     const data = JSON.stringify(rawData)
     const options = {
         method: 'PUT',
-        headers: {'Content-Type': 'application/json'},
+        headers: { 'Content-Type': 'application/json' },
         body: `{"id":"${id}","data":${data}}`
-      };
-      
-      const response = await fetch('http://localhost/operations', options).then(response => response.json())
-      return response
+    };
+
+    const response = await fetch('http://localhost/operations', options).then(response => response.json())
+    return response
 }
-export const createOpr = async({concept, amount, type}) =>{
+
+export const deleteOperation = async (id) => {
+    const options = { method: 'DELETE' };
+
+    const res = await fetch(`http://localhost/operations/${id}`, options).then(response => response.json())
+    return res
+}
+
+export const createOpr = async ({ concept, amount, type }) => {
     const user = '62d34380b3954d8270d05ea2'
     const options = {
         method: 'POST',
-        headers: {'Content-Type': 'application/json'},
+        headers: { 'Content-Type': 'application/json' },
         body: `{"concept":"${concept}","amount":${amount},"type":"${type}","user":"${user}"}`
-      };
-      
-      const res = fetch('http://localhost/operations', options).then(response => response.json())
-      return res
+    };
+
+    const res = fetch('http://localhost/operations', options).then(response => response.json())
+    return res
 
 }
 const sortOperations = (operations) => {
-    const data = operations.sort((a,b) => {
-        if(a.date < b.date) return 1
-        if(a.date > b.date) return -1
+    const data = operations.sort((a, b) => {
+        if (a.date < b.date) return 1
+        if (a.date > b.date) return -1
         return 0
     })
     return data
 }
-export const getLatest = async(user)  => {
+export const getLatest = async (user) => {
     const data = await getAll(user)
     const sortedData = sortOperations(data)
     return sortedData.slice(0, 10)
 }
 
-export async function getBalance(user){
+export async function getBalance(user) {
     const data = await getAll(user)
-    
+
     let balance = 0
     data.forEach(el => {
-        if(el.type === 'income'){
+        if (el.type === 'income') {
             balance += el.amount
-        }else{
+        } else {
             balance -= el.amount
         }
     });

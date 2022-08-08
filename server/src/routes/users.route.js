6ccb0e05e2f8b985000f1cf89fdfe3d67817ea09
Zaniuk <61323637+Zaniuk@users.noreply.github.com>
2022-08-08 20:58:44 -0300

    added users route

---
+++ server/src/routes/users.route.js
@@ -0,0 +1,14 @@
+import { Router } from "express";
+import { creeateUser, getUser, updateUser } from "../controllers/users.controller.js";
+const router = Router()
+
+router.get('/users/login', getUser)
+
+
+router.post('/users/register', creeateUser)
+
+router.put('/users/:id', updateUser)
+
+
+
+export default router

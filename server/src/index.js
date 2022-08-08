42f9977047e11fb1fa538f63fc0f11ca33319357
Zaniuk <61323637+Zaniuk@users.noreply.github.com>
2022-08-08 20:57:38 -0300

    added operations and users routes to index

--- server/src/index.js
+++ server/src/index.js
@@ -2,12 +2,13 @@ import 'dotenv/config'
 const PORT = process.env.PORT
 import express from 'express'
 import operationsRoute from './routes/operations.route.js'
+import usersRoute from './routes/users.route.js'
 import { sequelize } from './database/database.js'
 import './models/User.js'
 import  './models/Operation.js'
 const app = express()
 
-
+app.use(express.json())
 app.get('/', async(req, res) => {
   res.send('Hello World!')
   // try {
@@ -17,6 +18,8 @@ app.get('/', async(req, res) => {
   //   console.error('Unable to connect to the database:', error);
   // }
 })
+app.use(operationsRoute)
+app.use(usersRoute)
 
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}`)

9061316a31bfb203104225ef3f017d8d5867e710
Zaniuk <61323637+Zaniuk@users.noreply.github.com>
2022-08-08 20:58:31 -0300

    added operations controller

--- server/src/controllers/operations.controller.js
+++ server/src/controllers/operations.controller.js
@@ -1,9 +1,38 @@
-export const getOperations = (req, res) => {
-    res.send('Getting Operations')
+import { User } from "../models/User.js"
+import { Operation } from "../models/Operation.js"
+import { v1 as uuidv1 } from 'uuid';
+
+export const getOperations = async(req, res) => {
+    
+    const {token} = req.headers
+    const operations = await Operation.findAll({
+        where: {userId: token}
+    })
+    if(operations){
+        res.send(operations)
+    }else{
+        res.send({error: 'This user has not operations yet'})
+    }
+
+    
+    
 }
 
-export const createOperation = (req, res) => {
-    res.send('creating Operation')
+export const createOperation = async(req, res) => {
+    const {concept, amount, type} = req.body
+    const {token} = req.headers
+    if(concept && amount && type && token){
+        const operation = await Operation.create({
+            id: uuidv1(),
+            concept: concept,
+            amount: amount,
+            type: type, 
+            userId: token
+        })
+        res.send(operation)
+    }else{
+        res.send({error: "Please fill all fields"})
+    }
 }
 
 export const updateOperation = (req, res) => {

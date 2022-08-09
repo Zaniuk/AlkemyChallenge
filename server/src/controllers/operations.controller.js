c6ccf0dd8b6704099626bb5bc90984a3b6393195
Zaniuk <61323637+Zaniuk@users.noreply.github.com>
2022-08-09 18:10:33 -0300

    added getOperations controller & solved 43866ac

--- server/src/controllers/operations.controller.js
+++ server/src/controllers/operations.controller.js
@@ -1,53 +1,56 @@
 import { User } from "../models/User.js"
 import { Operation } from "../models/Operation.js"
-import { v1 as uuidv1 } from 'uuid';
+import { v4 as uuidv4 } from 'uuid';
 
-export const getOperations = async(req, res) => {
-    
-    const {token} = req.headers
+export const getOperations = async (req, res) => {
+
+    const { token } = req.headers
     const operations = await Operation.findAll({
-        where: {userId: token}
+        where: { userId: token }
     })
-    if(operations){
+    if (operations) {
         res.send(operations)
-    }else{
-        res.send({error: 'This user has not operations yet'})
+    } else {
+        res.send({ error: 'This user has not operations yet' })
     }
 
-    
-    
+
+
 }
 
-export const createOperation = async(req, res) => {
-    const {concept, amount, type} = req.body
-    const {token} = req.headers
-    if(concept && amount && type && token){
+export const createOperation = async (req, res) => {
+    const { concept, amount, type } = req.body
+    const { token } = req.headers
+    if (concept && amount && type && token) {
         const operation = await Operation.create({
-            id: uuidv1(),
+            id: uuidv4(),
             concept: concept,
             amount: amount,
-            type: type, 
+            type: type,
             userId: token
         })
         res.send(operation)
-    }else{
-        res.send({error: "Please fill all fields"})
+    } else {
+        res.send({ error: "Please fill all fields" })
     }
 }
 
-export const updateOperation = async(req, res) => {
-    const {id, data} = req.body
-    const {amount, concept} = data
-    if(data){
-        const operation = await Operation.findOne({
-            id: id
-        })
-        const newOperation = await operation.set({
-            amount, 
-            concept
-        }).save()
-        res.send(newOperation)
-
+export const updateOperation = async (req, res) => {
+    const { id, data } = req.body
+    const { amount, concept } = data
+    const { token } = req.headers
+    if (data) {
+        try {
+            const operation = await Operation.update({
+                amount,
+                concept
+            }, {
+                where: { userId: token, id: id }
+            })
+            res.send(operation)
+        } catch (error) {
+            res.send({ error })
+        }
     }
 
 }

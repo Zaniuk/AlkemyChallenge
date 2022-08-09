edf3be49203aca3f48538023b6d2393c4c0ee9cc
Zaniuk <61323637+Zaniuk@users.noreply.github.com>
2022-08-09 18:11:28 -0300

    changed login to post request

--- server/src/routes/users.route.js
+++ server/src/routes/users.route.js
@@ -2,7 +2,7 @@ import { Router } from "express";
 import { creeateUser, getUser, updateUser } from "../controllers/users.controller.js";
 const router = Router()
 
-router.get('/users/login', getUser)
+router.post('/users/login', getUser)
 
 
 router.post('/users/register', creeateUser)

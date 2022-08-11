4d1fd8b1a0595686f5d5608017d47cd93073c8be
Zaniuk <61323637+Zaniuk@users.noreply.github.com>
2022-08-11 11:29:10 -0300

    added categories declared in db enum

--- client/src/helpers/chartHelper.js
+++ client/src/helpers/chartHelper.js
@@ -1,37 +1,42 @@
 import { getAll } from "./helpers";
 export const monthlyOperationsResume = async (user) => {
     const operationList = await getAll(user)
-    let Food = 0
-    let Games = 0
+    let Services = 0
+    let Market = 0
     let Entertainment = 0
-    let Taxes = 0
+    let Shopping = 0
+    let Others = 0
     operationList.forEach((op) => {
         switch (op.concept) {
-            case 'Food':
-                Food += op.amount
+            case 'Services':
+                Services += op.amount
                 break;
-            case 'Games':
-                Games += op.amount
+            case 'Market':
+                Market += op.amount
                 break;
             case 'Entertainment':
                 Entertainment += op.amount
                 break;
-            case 'Taxes':
-                Taxes += op.amount
+            case 'Shopping':
+                Shopping += op.amount
+                break;
+            case 'Others':
+                Others += op.amount
                 break;
         }
     })
 
     const res = {
-        data: [Food, Games, Entertainment, Taxes],
+        data: [Services, Market, Entertainment, Shopping, Others],
         labels:
             [
-                'Food',
-                'Games',
+                'Services',
+                'Market',
                 'Entertainment',
-                'Taxes'
+                'Shopping',
+                'Others'
             ],
-        colors: ['#14d0f0', '	#7f73ff', '#ff83c3', '#ffe552']
+        colors: ['#14d0f0', '#7f73ff', '#ff83c3', '#ffe552', '#ffe552']
     }
     return res
 

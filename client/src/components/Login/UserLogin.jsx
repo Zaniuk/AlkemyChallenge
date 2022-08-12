a5cb5e35f0e93603adc233feb86ce1f17b37061c
Zaniuk <61323637+Zaniuk@users.noreply.github.com>
2022-08-12 10:08:59 -0300

    first 'good' version of login/auth

--- client/src/components/Login/UserLogin.jsx
+++ client/src/components/Login/UserLogin.jsx
@@ -1,9 +1,17 @@
 import React from 'react'
-
+import { login } from '../../helpers/helpers'
 export default function UserLogin() {
+  const signIn = (e) => {
+    e.preventDefault()
+    login('gerozwa49@g12312mail.com', 'callof1dutymw32').then(user =>{
+      if(user){
+        location.replace('/')
+      }
+    })
+  } 
   return (
     <div className="container">
-      <form>
+      <form onSubmit={signIn}>
         <label>
           Email
           <input type="email" required />

999a49eb440dd3c6abeb43645ff22f116c8aa746
Zaniuk <61323637+Zaniuk@users.noreply.github.com>
2022-08-14 20:33:46 -0300

    improved readbility

--- client/src/components/Dashboard/Table/TableItem.jsx
+++ client/src/components/Dashboard/Table/TableItem.jsx
@@ -1,15 +1,25 @@
-import React, { useEffect, useState } from 'react'
+import React from 'react'
 import { convertDateToStriing, deleteOperation } from '../../../helpers/helpers'
-export default function TableItem({ operation }) {
-  const token = '950620a4-9a95-4a1e-b34d-470d5096498a'
+
+
+export default function TableItem({ operation , colorClass}) {
+  
+  const token = sessionStorage.getItem('token')
+  const setOperationSymbol = (operation) => {
+    if(operation.type === 'income'){
+        return `+${operation.amount}`
+      }else{
+        return `-${operation.amount}`
+      }
+    }
+  
   if (operation) {
     const date = convertDateToStriing(operation.date)
     return (
       <tr id={operation._id}>
         <td>{date}</td>
-        <td>{operation.type}</td>
         <td>{operation.concept}</td>
-        <td>{operation.amount}</td>
+        <td className={colorClass}>{setOperationSymbol(operation)}</td>
         <td>
           <a href={`edit/${operation.id}&${operation.type}`}>
             <button>Editar</button>

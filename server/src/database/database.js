8cb7ea1657b1c4002c0822e53cd01c50c20f152b
Zaniuk <61323637+Zaniuk@users.noreply.github.com>
2022-11-18 20:40:02 -0300

    migrate to sqlite

--- server/src/database/database.js
+++ server/src/database/database.js
@@ -1,5 +1,9 @@
 import { Sequelize } from "sequelize"
-export const sequelize = new Sequelize(process.env.DB_LOCAL_NAME, process.env.DB_LOCAL_USERNAME, process.env.DB_LOCAL_PASSWORD, {
-    host: process.env.DB_LOCAL_HOST,
-    dialect: 'postgres'
+// export const sequelize = new Sequelize(process.env.DB_LOCAL_NAME, process.env.DB_LOCAL_USERNAME, process.env.DB_LOCAL_PASSWORD, {
+//     host: process.env.DB_LOCAL_HOST,
+//     dialect: 'postgres'
+// })
+export const sequelize = new Sequelize({
+    dialect: 'sqlite',
+    storage: '../../abm.db'
 })

1cb4f65a8194a55d32d50c270a4824fcb84507e0
Zaniuk <61323637+Zaniuk@users.noreply.github.com>
2022-11-21 17:36:28 -0300

    add auth/me endpoint

--- server/src/routes/users.route.js
+++ server/src/routes/users.route.js
@@ -1,13 +1,13 @@
 import { Router } from "express";
-import { creeateUser, getUser, updateUser } from "../controllers/users.controller.js";
+import { creeateUser, login , auth} from "../controllers/users.controller.js";
 const router = Router()
 
-router.post('/users/login', getUser)
-
+router.post('/users/login', login)
 
 router.post('/users/register', creeateUser)
 
-router.put('/users/:id', updateUser)
+router.get('/auth/me' , auth)
+
 
 
 

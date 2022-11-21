013e3409e2e788edcafe7960f94f53a3eb14c9bd
Zaniuk <61323637+Zaniuk@users.noreply.github.com>
2022-11-21 18:12:01 -0300

    create hash password helper

--- server/src/helpers/users.js
+++ server/src/helpers/users.js
@@ -1,10 +1,13 @@
 import bcrypt from "bcrypt";
 import jwt from "jsonwebtoken";
 export const isValidPassword = async (userpass, password) => {
-  return await bcrypt.compareSync(password, userpass);
+  return await bcrypt.compareSync(String(password), String(userpass));
 };
 export const signToken = async (id) => {
   return await jwt.sign({ id }, process.env.JWT_SECRET, {
     expiresIn: 86400, // 24 hours
   });
 };
+export const hashPassword = async (password) => {
+    return await bcrypt.hashSync(String(password), 10);
+    }

52d6db12ca5738815857481526ad8892dd296e66
Zaniuk <61323637+Zaniuk@users.noreply.github.com>
2022-11-21 17:35:18 -0300

    created a dao for each operation endpoint

---
+++ server/src/controllers/dao.js
@@ -0,0 +1,33 @@
+import { Operation } from "../models/Operation.js";
+
+export const operationsDao = {
+    getOperations: async (id) => {
+        const operations = await Operation.findAll({
+            where: {
+                userId: id
+            }
+        });
+        return operations;
+    },
+    getOperationById: async (id) => {
+        const operation = await Operation.findOne({
+            where: {
+                id: id
+            }
+        });
+        return operation;
+    },
+    createOperation: async (data) => {
+        const operation = await Operation.create(data);
+        return operation;
+    },
+    updateOperation: async (data, id, userId) => {
+        const operation = await Operation.update(data,{
+            where: {
+                id: id,
+                userId: userId,
+            }
+        })
+        return operation;
+    }
+}

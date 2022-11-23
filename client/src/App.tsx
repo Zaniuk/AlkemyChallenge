c81463e952cf6b5d20adb485c0402865e33446ab
Zaniuk <61323637+Zaniuk@users.noreply.github.com>
2022-11-22 21:18:52 -0300

    moved UserProvider to App

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,9 +1,22 @@
-import React from 'react';
-import Router from './Router';
+import React from "react";
+import Router from "./Router";
+import { ThemeProvider, createTheme } from "@mui/material/styles";
+import CssBaseline from "@mui/material/CssBaseline";
+import UserProvider from "./context/UserProvider";
+const darkTheme = createTheme({
+  palette: {
+    mode: "dark",
+  },
+});
 
 function App() {
   return (
-    <Router/>
+    <ThemeProvider theme={darkTheme}>
+      <CssBaseline />
+      <UserProvider>
+        <Router />
+      </UserProvider>
+    </ThemeProvider>
   );
 }
 
